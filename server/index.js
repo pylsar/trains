afbd87af8fd21b4463d16fa0d8c056fc76d7055a
Alex Kusov <kysov2006@bk.ru>
2022-01-20 06:24:38 +0300

    mongo works

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ const posts = require('./routes/api/posts.js');
 
 app.use('/api/posts', posts);
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`server starts at port ${5000}`)
+app.listen(PORT, () => {
+    console.log(`server starts at port http://localhost/${PORT}`)
 })
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -11,13 +11,26 @@ router.get('/', async (req, res) => {
 })
 
 //Add Posts
+router.post('/', async (req, res ) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+})
+
 
 //Delete Posts
-
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+})
 
 async function loadPostsCollection(){
-    const client = mongodb.MongoClient.connect('mongodb+srv://train:ParolHeroes@cluster0.bu2wa.mongodb.net/poezda?retryWrites=true&w=majority', {
-        useNewUrlParser: true
+    const client = await mongodb.MongoClient.connect('mongodb+srv://UserTrain:ParolHeroes@cluster0.bu2wa.mongodb.net/poezda?retryWrites=true&w=majority', {
+        useNewUrlParser: true // to protect us from warnings
     });
     return client.db('trains').collection('posts')
 }
